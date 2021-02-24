5ba8014ba199ad37a53bbddf822e78c8b5b9b8fd
Deltrum <deltrum@outlook.com>
2021-02-24 11:47:15 +0300

    Base client application added

---
+++ client/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="wrapper">
+        <Header />
+        <Nuxt />
+    </div>
+</template>
+
+<script lang="ts" >
+import { Vue, Component } from "vue-property-decorator";
+import Header from "@/components/Header.vue";
+
+@Component({ name: "Wrapper", props: { Header } })
+export default class Wrapper extends Vue {}
+</script>
---
+++ client/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <main class="home main">
+        <div class="container">
+            <div class="home__inner"></div>
+        </div>
+    </main>
+</template>
+
+<script lang="ts" >
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({ name: "Home" })
+export default class Home extends Vue {}
+</script>
